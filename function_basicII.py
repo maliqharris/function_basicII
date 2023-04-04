0bebb231b37a79a14ec432f56076cbc220b90f2c
maliqharris <127056176+maliqharris@users.noreply.github.com>
2023-04-04 02:18:52 -0400

    Function basic II ! Create some functions using Python!

---
+++ function_basicII.py
@@ -0,0 +1,39 @@
+# def countDown(x):
+#     for i in range (x, -1, -1):
+#         print(i)
+# countDown(5)
+
+# def printReturn(list):
+#     print(list[0])
+#     return(list[1])
+# printReturn([1,2])
+
+# def firPlusLen(list):
+#     sum = list[0] + len(list)
+#     print(sum)
+#     return(sum)
+# firPlusLen([1,2,3,4,5])
+
+# def valGreatSec(list):
+#     if len(list) > 2:
+#         list2 = [] 
+#         for i in list :
+#             if i > list[1]:
+#                 list2.append(i)
+#         print(len(list2))
+#         return list2
+#     else:
+#         print("False") 
+#         return "False"
+# valGreatSec([5,2,3,2,1,4])
+        
+# def LenVal(size,value):
+#     ourlistyo = []
+#     for i in range (size+1):
+#         ourlistyo.append(value)
+#     print(ourlistyo) 
+#     return(ourlistyo)   
+
+    
+# LenVal(4,7)
+
